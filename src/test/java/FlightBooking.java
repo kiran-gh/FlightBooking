7b26280234872db7dcbb703a32088cc97a5c3a45
Satyakiran <satyakiran.vogeti@gmail.com>
2024-08-29 20:49:14 +0530

    Flight booking application code is updated and comments added. README file generated with full description about this project

--- src/test/java/FlightBooking.java
+++ src/test/java/FlightBooking.java
@@ -1,18 +1,42 @@
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class FlightBooking extends driverFactory{
+    WebDriverWait wait;
+    By searchFlightsButtonEle = By.id("searchBtn");
+    By selectFlightEle = By.xpath("(//input[@type=\"radio\"])[1]");
+    By bookNowButtonEle = By.tagName("button");
+    By enterPasswordEle = By.xpath("//input[@type=\"password\"]");
+    By headingElement = By.tagName("h2");
+
+
     @BeforeMethod
     public void launchChromeBrowser() {
         launchBrowser();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(35));
     }
+
+
+    public void clickOnAnElement(By locator){
+        WebElement element = driver.findElement(locator);
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.click();
+    }
+
     @Test(priority = 1)
     public void FlightBookingPageTest() throws InterruptedException, IOException {
         driver.get("https://qaflightbooking.ccbp.tech/");
@@ -22,16 +46,24 @@ public class FlightBooking extends driverFactory{
         driver.findElement(By.id("travelDate")).sendKeys("01/08/2023");
         driver.findElement(By.id("passengers")).sendKeys("2");
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+        clickOnAnElement(searchFlightsButtonEle);
+        clickOnAnElement(selectFlightEle);
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//input[@value=\"0\"]")).click();
+        String availableFlightsHeading = driver.findElement(headingElement).getText();
+//        checks if available flights heading is available or not
+        Assert.assertEquals(availableFlightsHeading,"Available Flights","Available flights heading miss-matching ");
+        clickOnAnElement(bookNowButtonEle);
+        String paymentDetailsHeading = driver.findElement(headingElement).getText();
+//        checks if payment details heading is available or not
+        Assert.assertEquals(paymentDetailsHeading,"Payment Details","Payment details heading miss-matching ");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(enterPasswordEle));
+        driver.findElement(enterPasswordEle).sendKeys("traveler123");
+        clickOnAnElement(bookNowButtonEle);
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//button[normalize-space()=\"Book Now\"]")).click();
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("//input[@placeholder=\"Enter password\"]")).sendKeys("traveler123");
-        driver.findElement(By.xpath("//button[normalize-space()=\"Pay Now\"]")).click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        String bookingSuccessHeading = driver.findElement(headingElement).getText();
+//        checks if booking success heading is available or not
+        Assert.assertEquals(bookingSuccessHeading,"Booking Success!","Booking success heading miss-matching ");
+//        Taking Screen shot
         File bookingSuccess = driver.findElement(By.className("container")).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(bookingSuccess, new File(".//Screenshot/" + "bookingSuccess" + ".png"));
     }
